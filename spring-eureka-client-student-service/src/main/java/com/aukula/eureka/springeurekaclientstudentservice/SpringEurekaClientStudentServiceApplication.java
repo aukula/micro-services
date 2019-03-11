7fec978eb91ce6d0a051cc6082f4310b0699d04e
Vivekananda Aukula <vivekananda.aukula@outlook.com>
2019-03-11 12:59:12 +0530

    Added Zuul proxy,eureka

---
+++ spring-eureka-client-student-service/src/main/java/com/aukula/eureka/springeurekaclientstudentservice/SpringEurekaClientStudentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aukula.eureka.springeurekaclientstudentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaClientStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaClientStudentServiceApplication.class, args);
+	}
+
+}
